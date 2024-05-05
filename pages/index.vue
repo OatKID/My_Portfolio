0f2d8885e9d3428e794e9c20e1fa5e6b9804bd33
Siwakorn Pasawang <pasawang.sivakorn@gmail.com>
2024-05-05 21:29:05 +0700

    Add header navbar

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="Header">
+        <div class="NavBar">
+            <div class="leftBar">My Porfolio</div>
+			<div class="rightBar">time</div>
+        </div>
+        <div class="HomeImg">
+            <img src="/assets/img/homepage.jpg"/>
+            <div class="profile">
+                Hi, I'm Siwakorn Pasawang (Oat)
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    
+</script>
