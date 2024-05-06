e573a71f4120b6fffbda2d5b1346a24b6d25e724
Siwakorn Pasawang <pasawang.sivakorn@gmail.com>
2024-05-06 22:13:53 +0700

    add more detail of header

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,21 @@
         <div class="HomeImg">
             <img src="/assets/img/homepage.jpg"/>
             <div class="profile">
-                Hi, I'm Siwakorn Pasawang (Oat)
+                <div class="speech">
+                    <h2>
+                        <span>Hi</span>
+                        <br>
+                        <br>
+                        I'm Siwakorn Pasawang (Oat).
+                        <br>
+                        <br>
+                        I achieve my projects !!
+                        
+                    </h2>
+                </div>
+                <div class="profileImg">
+                    <img src="~/assets/img/profile.jpg" alt="" srcset="">
+                </div>
             </div>
         </div>
     </div>
